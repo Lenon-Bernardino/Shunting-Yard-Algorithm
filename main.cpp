e87f21b9664deacf6f7e8624f35302b62dff2961
Lenon Saturnino Bernardino <70743202+Lenon-Bernardino@users.noreply.github.com>
2022-09-03 19:31:00 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -38,6 +38,41 @@ char checktype(char check_str)
     return 'i';
 }
 
+char checktype_str(string check_str)
+{
+    string numbers[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+    string operators[] = {"+", "-", "/", "*"};
+    const string Lparen = "(";
+    const string Rparen = ")";
+
+    for(int i = 0; i < sizeof(numbers); i++)
+    {
+        const string current = numbers[i];
+        if(check_str == Lparen)
+        {
+            return '(';
+        }
+
+        if(check_str == Rparen)
+        {
+            return ')';
+        }
+
+        if(check_str == numbers[i])
+        {
+            return 'n';
+        }
+
+        if(i <= sizeof(operators))
+        {
+            if(check_str == operators[i])
+            {
+                return 'o';
+            }
+        }
+    }
+}
+
 bool checkoperator(char check_str)
 {
     char numbers[] = {'+', '-', '/', '*'};
@@ -82,6 +117,25 @@ string display_vector(vector<char> thing)
     string display = "{";
     for(int i = 0; i < thing.size(); i++)
     {
+        string s(1, thing.at(i));
+        if(i == 0)
+        {
+            display += s;
+        }
+        else
+        {
+            display += "," + s;
+        }
+    }
+    display += "}";
+    return display;
+}
+
+string display_str_vector(vector<string> thing)
+{
+    string display = "{";
+    for(int i = 0; i < thing.size(); i++)
+    {
         if(i == 0)
         {
             display += thing.at(i);
@@ -102,31 +156,42 @@ int main()
     cin >> input;
     
     vector<char> operator_stack;
-    vector<char> output_queue;
+    string buffer_for_stuff = "";
+    vector<string> output_queue;
     char token_type;
+    char new_token_type;
 
     const char number = 'n';
     const char operator_ = 'o';
     const char left_paren = '(';
     const char right_paren = ')';
-
     for(int i = 0; i < input.length(); i++)
     {
+        string current_charSTR(1, input[i]);
+        new_token_type = checktype(input[i]);
+        if(new_token_type == 'n' && token_type == 'n')
+        {
+            output_queue.back() = output_queue.back() + current_charSTR;
+            continue;
+        }
+
         token_type = checktype(input[i]);
         cout << "\nOperator stack: " << display_vector(operator_stack);
-        cout << "\nOutput queue: " << display_vector(output_queue);
+        cout << "\nOutput queue: " << display_str_vector(output_queue);
         cout << "\nToken type: " << token_type;
+        cout << "\ninput: " << input[i];
         cout << "\n==================================================================";
         switch (token_type)
         {
             case number:
-                output_queue.push_back(input[i]);
+                output_queue.push_back(current_charSTR);
                 continue;
             case operator_:
                 // Code stops inside this while loop, need to implement this differently.
                 while(operator_with_greater_precedence(operator_stack, input[i]))
                 {
-                    output_queue.push_back(operator_stack.back());
+                    buffer_for_stuff = string(1, operator_stack.back());
+                    output_queue.push_back(buffer_for_stuff);
                     operator_stack.pop_back(); // Popping the last from left to right, because the top of the stack is the last from left to right.
                 }
                 operator_stack.push_back(input[i]); // Pushing it into the vector from right to left.
@@ -137,7 +202,8 @@ int main()
             case right_paren:
                 while(operator_stack.back() != left_paren)
                 {
-                    output_queue.push_back(operator_stack.back());
+                    buffer_for_stuff = string(1, operator_stack.back());
+                    output_queue.push_back(buffer_for_stuff);
                     operator_stack.pop_back();
                 }
                 operator_stack.pop_back(); // popping the left bracket cause we couldn't do it in the while loop
@@ -146,10 +212,114 @@ int main()
     }
     while(operator_stack.size() != 0)
     {
-        output_queue.push_back(operator_stack.back());
+        buffer_for_stuff = string(1, operator_stack.back());
+        output_queue.push_back(buffer_for_stuff);
         operator_stack.pop_back();
     }
     cout << "\nOperator stack: " << display_vector(operator_stack);
-    cout << "\nOutput queue: " << display_vector(output_queue);
+    cout << "\nOutput queue: " << display_str_vector(output_queue);
     cout << "\nToken type: " << token_type;
+
+    vector<string> operators = {};
+    vector<string> two_numbers = {};
+    string current_str;
+    int first;
+    int second;
+    int result;
+    string current_operator;
+    for(int i = output_queue.size()-1; i <= output_queue.size()-1; i--)
+    {
+        current_str = output_queue.at(i);
+        cout << "\nOperators: " << display_str_vector(operators);
+        cout << "\nOutput queue: " << display_str_vector(output_queue);
+        cout << "\nToken type: " << token_type;
+        cout << "\nCurrent token: " << current_str;
+        cout << "\nTwo numbers: " << display_str_vector(two_numbers);
+        cout << "\ni = " << i;
+        cout << "\n==================================================================";
+        cout << "\nhere 1";
+        if(current_str.length() > 1)
+        {
+            token_type = 'n';
+        }
+        else
+        {
+            token_type = checktype_str(current_str);
+        }
+        cout << "\nhere 2";
+        if(token_type == 'o')
+        {
+            cout << "\nhere 3";
+            operators.push_back(current_str);
+            output_queue.pop_back();
+        }
+        else
+        {
+            cout << "\nhere 4";
+            two_numbers.push_back(current_str);
+            cout << "\nhere 5";
+        }
+
+        if(two_numbers.size() == 2)
+        {
+            first = stoi(two_numbers.back()); // (9)
+            second = stoi(two_numbers.front()); // (3)
+            cout << "\nfirst: " << first;
+            cout << "\nsecond: " << second;
+            current_operator = operators.back();
+            if(current_operator == "+")
+            {
+                result = first+second;
+                two_numbers.pop_back();
+                two_numbers.pop_back();
+                output_queue.at(i) = to_string(result);
+                output_queue.pop_back();
+                operators.pop_back();
+                i += 1;
+                continue;
+            }
+            if(current_operator == "-")
+            {
+                result = first-second;
+                two_numbers.pop_back();
+                two_numbers.pop_back();
+                output_queue.at(i) = to_string(result);
+                output_queue.pop_back();
+                operators.pop_back();
+                i += 1;
+                continue;
+            }
+            if(current_operator == "*")
+            {
+                result = first*second;
+                two_numbers.pop_back();
+                two_numbers.pop_back();
+                output_queue.at(i) = to_string(result);
+                output_queue.pop_back();
+                i += 1;
+                operators.pop_back();
+                continue;
+            }
+            if(current_operator == "/")
+            {
+                result = first/second;
+                two_numbers.pop_back();
+                two_numbers.pop_back();
+                output_queue.at(i) = to_string(result);
+                output_queue.pop_back();
+                i += 1;
+                operators.pop_back();
+                continue;
+            }
+        }
+    }
+    cout << "\nOperators: " << display_str_vector(operators);
+    cout << "\nOutput queue: " << display_str_vector(output_queue);
+    cout << "\nToken type: " << token_type;
+    cout << "\nCurrent token: " << current_str;
+    cout << "\nTwo numbers: " << display_str_vector(two_numbers);
+    cout << "\n==================================================================";
+
+    string final_result = output_queue.back();
+    cout << "\n\nFINAL RESULT: " << final_result;
 }
